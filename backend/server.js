c9520d47241882a66b85279e74180ed51c8a60cb
Bhumika2101 <bhumikasalunkhe283@gmail.com>
2026-02-07 13:26:22 +0530

    frontnend prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 // Configure CORS for Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['https://work-grid-five.vercel.app/', 'http://localhost:3000'],
     methods: ['GET', 'POST'],
     credentials: true
   }
